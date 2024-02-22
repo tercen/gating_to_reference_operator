534de15db992b3cca1e8e03ae9f9963d2a4cc2ac
agouy <alexandre.gouy@tercen.com>
2024-02-22 08:20:16 +0000

    Initial version

--- main.R
+++ main.R
@@ -1,11 +1,70 @@
 library(tercen)
-library(dplyr, warn.conflicts = FALSE)
+library(dplyr)
+source("./utils.R")
 
-ctx = tercenCtx()
+ctx <- tercenCtx()
 
-ctx %>%
-  select(.y, .ci, .ri) %>% 
-  group_by(.ci, .ri) %>%
-  summarise(mean = mean(.y)) %>%
-  ctx$addNamespace() %>%
-  ctx$save()
+ds_gating <- get_gating_step(ctx, id = 1)
+gts <- ds_gating$model$operatorSettings$operatorModel$roots
+
+resultsList <- lapply(gts, applyRecursively)
+
+flattenedResults <- flattenList(resultsList) %>% as.data.frame()
+flattenedResults$x_parent <- c(NA, unlist(sapply(flattenedResults[, "parent"], function(x) flattenedResults$x_name[flattenedResults$gate_name == x])))
+flattenedResults$y_parent <- c(NA, unlist(sapply(flattenedResults[, "parent"], function(x) flattenedResults$y_name[flattenedResults$gate_name == x])))
+flattenedResults$has_children <- flattenedResults$gate_name %in% flattenedResults$parent
+
+df_out <- lapply(flattenedResults, unlist) %>% as_tibble
+
+mat <- ctx$as.matrix()
+df_col <- ctx$cselect() %>% tidyr::unite(col = "col_names")
+df_row <- ctx$rselect() %>% tidyr::unite(col = "row_names")
+colnames(mat) <- df_col$col_names
+rownames(mat) <- df_row$row_names
+
+scaled <- apply(mat, 2, function(x) {
+  min_val <- min(x)
+  max_val <- max(x)
+  scaled <- (x - min_val) / (max_val - min_val) * 2 - 1
+  return(scaled)
+})
+
+# Get and filter out unused markers
+edges_df <- data.frame(
+  parent = df_out$parent,
+  child = df_out$gate_name
+)
+paths <- edgeListToPaths(edges_df)
+
+paths_filled <- apply(paths, 1, fill_na_with_repetition) %>% t()
+
+# get, for each leaf, the list of markers
+mk_list <- apply(paths_filled, 1, function(x) {
+  df_out %>% 
+    filter(gate_name %in% unique(x)) %>%
+    select(x_parent, y_parent) %>%
+    unlist() %>%
+    unique()
+})
+
+
+# Mask data (option)
+sc_out <- scaled[idx, ]
+idx <- paste0(ds_gating$model$operatorSettings$namespace, ".", paths_filled[, ncol(paths_filled)])
+cn <- unlist(lapply(strsplit(colnames(sc_out), "\\."), "[", 2))
+for(i in 1:nrow(paths_filled)) {
+  sc_out[i, !colnames(sc_out) %in% mk_list[[i]]] <- NA
+}
+colnames(sc_out) <- cn
+
+
+colnames(paths_filled)[length(colnames(paths_filled))] <- "Population"
+out <- bind_cols(as_tibble(paths_filled), as_tibble(sc_out[idx, ]))
+
+out %>%
+  tidyr::pivot_longer(!contains(c("Level", "Population")), names_to = "Marker", values_to = "Value") %>%
+  # ctx$addNamespace() %>%
+  as_relation() %>%
+  as_join_operator(list(), list()) %>%
+  save_relation(ctx)
+
---
+++ utils.R
@@ -0,0 +1,119 @@
+
+#####
+### Workflow queries
+get_workflow_id <- function(ctx) {
+  if (is.null(ctx$task)) {
+    return(ctx$workflowId)
+  } else {
+    workflowIdPair <-
+      Find(function(pair)
+        identical(pair$key, "workflow.id"),
+        ctx$task$environment)
+    workflowId <- workflowIdPair$value
+    return(workflowId)
+  }
+}
+
+get_step_id <- function(ctx) {
+  if (is.null(ctx$task)) {
+    return(ctx$stepId)
+  } else {
+    stepIdPair <-
+      Find(function(pair)
+        identical(pair$key, "step.id"),
+        ctx$task$environment)
+    stepId <- stepIdPair$value
+    return(stepId)
+  }
+}
+
+
+get_data_step <- function(ctx) {
+  wf <- ctx$client$workflowService$get(get_workflow_id(ctx))
+  ds <-
+    Find(function(s)
+      identical(s$id, get_step_id(ctx)), wf$steps)
+  return(ds)
+}
+
+get_gating_step <- function(ctx, id = 1) {
+  wf <- ctx$client$workflowService$get(get_workflow_id(ctx))
+  ds <- Find(function(s) !is.null(s$model$operatorSettings$operatorModel$roots), wf$steps)[[id]]
+  return(ds)
+}
+
+# Function to recursively traverse the tree and collect paths
+collect_paths <- function(edges_df, node, current_path) {
+  current_path <- c(current_path, node)
+  
+  children <- edges_df$child[edges_df$parent == node]
+  
+  if (length(children) == 0) {
+    return(list(current_path))
+  } else {
+    all_paths <- list()
+    for (child in children) {
+      all_paths <- c(all_paths, collect_paths(edges_df, child, current_path))
+    }
+    return(all_paths)
+  }
+}
+
+edgeListToPaths <- function(edges_df) {
+
+  root_nodes <- setdiff(unique(edges_df$parent), edges_df$child)
+  
+  all_paths <- list()
+  for (root in root_nodes) {
+    all_paths <- c(all_paths, collect_paths(edges_df, root, character(0)))
+  }
+  
+  max_length <- max(sapply(all_paths, length))
+  paths_df <- do.call(rbind, lapply(all_paths, function(x) c(x, rep(NA, max_length - length(x)))))
+  
+  colnames(paths_df) <- paste0("Level_", 1:max_length)
+  return(as.data.frame(paths_df))
+}
+
+getGateNodeFactors <- function(GateNode, parent) {
+  x_name <- GateNode$crosstab$axis$xyAxis[[1]]$xAxis$graphicalFactor$factor$name
+  y_name <- GateNode$crosstab$axis$xyAxis[[1]]$yAxis$graphicalFactor$factor$name
+  return(c(gate_name = GateNode$name, parent = parent, x_name = x_name, y_name = y_name))
+}
+
+applyRecursively <- function(GateNode, parent = "root") {
+  result <- getGateNodeFactors(GateNode, parent)
+  results <- list(result)
+  
+  # Recursively apply the function to all children
+  if (!is.null(GateNode$children)) {
+    childResults <- lapply(GateNode$children, applyRecursively, parent = GateNode$name)
+    results <- c(results, childResults)
+  }
+  return(results)
+}
+
+flattenList <- function(lst) {
+  out <- list()
+  for (i in seq_along(lst)) {
+    if (is.list(lst[[i]])) {
+      out <- rbind(out, flattenList(lst[[i]]))
+    } else {
+      out <- rbind(out, lst[[i]])
+    }
+  }
+  return(out)
+}
+
+
+fill_na_with_repetition <- function(x) {
+  last_non_na <- NULL
+  for (i in 1:length(x)) {
+    if (!is.na(x[i])) {
+      last_non_na <- x[i]
+    } else if (!is.null(last_non_na)) {
+      x[i] <- last_non_na
+    }
+  }
+  return(x)
+}
